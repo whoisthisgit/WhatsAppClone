c61beb5a072a6061cc56236739b0c7282091d2e4
Craig School Computer Lab User <openlab@fresnostate.edu>
2017-04-27 12:42:16 -0700

    finished Section1

---
+++ client/imports/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { MomentModule } from 'angular2-moment';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { ChatsPage } from '../pages/chats/chats'
+import { MessagesPage } from '../pages/messages/messages';
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ChatsPage,
+    MessagesPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    MomentModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ChatsPage,
+    MessagesPage
+  ],
+  providers: [
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
